c6d982dba7c3801ec56dc6a56753ea0aee17c596
Android Studio <devandroid@ort>
2025-05-19 10:29:01 -0300

    correcciones + tipografía

--- NutraMobile/App.js
+++ NutraMobile/App.js
@@ -1,25 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View, TextInput, Image, ImageBackground, SafeAreaView } from 'react-native';
 import React from 'react';
+import { useFonts } from 'expo-font';
 
+//npx expo install expo-font 
 
+const imgPerfil = {uri: 'https://www.floatingwindturbineucm.com/wp-content/uploads/PERFIL-VACIO-1024x1024.png'}
 export default function App() {
-  const fondo = require('./assets/fondo-login.png')
+
+  useFonts({
+    'Inter': require('./assets/fonts/Inter/Inter_18pt-Regular.ttf')
+  });
+
+  
   const logo = require('./assets/LOGO-transparente.png')
   return (
     <SafeAreaView style={styles.container}>
-      <Image
-      source = {logo}
-      />
+      
       <StatusBar style="auto" />
 
-      <View style={styles.container}>
-      
-      <TextInput
-          style={styles.input}
-          placeholder="Ingrese su Usuario"
-        />
+      <Text style={styles.titulo}>Perfil</Text> 
+      <Image source={imgPerfil} resizeMode= 'cover' style={styles.fotoPerfil}></Image>
+      <View style={styles.User}>
+       
       </View>
+      
     </SafeAreaView>
     
   );
@@ -32,4 +37,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo:{
+    flex: 3,
+    fontFamily: 'Inter',
+    fontSize: 24,
+    margin: 50
+  },
+  fotoPerfil:{
+    flex: 1,
+    justifyContent: 'center'
+  },
+
 });
