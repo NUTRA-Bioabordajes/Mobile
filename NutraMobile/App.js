2d39b1ef930e2d62eee23e4905fc634025bf3777
Android Studio <devandroid@ort>
2025-05-23 11:49:42 -0300

    info personal del usuario

--- NutraMobile/App.js
+++ NutraMobile/App.js
@@ -4,8 +4,9 @@ import React from 'react';
 import { useFonts } from 'expo-font';
 
 //npx expo install expo-font 
-
-const imgPerfil = {uri: 'https://www.floatingwindturbineucm.com/wp-content/uploads/PERFIL-VACIO-1024x1024.png'}
+const logo = require('./assets/images/LOGO-transparente.png')
+const imgPerfil = {uri: 'https://www.floatingwindturbineucm.com/wp-content/uploads/PERFIL-VACIO-1024x1024.png'};
+const iconoUbi = require('./assets/icons/iconoUbicacion.png')
 export default function App() {
 
   useFonts({
@@ -13,18 +14,28 @@ export default function App() {
   });
 
   
-  const logo = require('./assets/LOGO-transparente.png')
   return (
     <SafeAreaView style={styles.container}>
       
       <StatusBar style="auto" />
 
       <Text style={styles.titulo}>Perfil</Text> 
-      <Image source={imgPerfil} resizeMode= 'cover' style={styles.fotoPerfil}></Image>
-      <View style={styles.User}>
-       
-      </View>
       
+      
+      <View style={styles.user}>
+        <Image source={imgPerfil} resizeMode='contain' style={styles.fotoPerfil}/>
+        <View style = {{margin: '20px'}}> 
+          <Text style={styles.nombre}>Felipe</Text> 
+          <Text style={styles.edad}>8 años</Text>
+          <View style = {styles.barrio}>
+            <Image source={iconoUbi} resizeMode='contain'/>
+            <Text style={styles.edad}>Caballito</Text>
+          </View>  
+           
+        </View>
+      </View>
+
+    
     </SafeAreaView>
     
   );
@@ -38,14 +49,39 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   titulo:{
-    flex: 3,
+    //flex: 3,
     fontFamily: 'Inter',
     fontSize: 24,
     margin: 50
   },
   fotoPerfil:{
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    width: '120px',
+    height: '120px',
+    flexDirection: 'row'
   },
+  user:{
+    flexDirection: 'row',
+   
+
+  },
+  nombre:{
+    fontFamily: 'Inter',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: '20px',
+    margin: '5px'
+  },
+  edad:{
+    fontFamily: 'Inter',
+    fontSize: 18,
+    //opacity: '0.6',
+    margin: '5px'
+
+  },
+  barrio:{
+    flexDirection: 'row'
+  }
 
 });
