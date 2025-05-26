ca79e72e5d3e4b5088c4e2469945fa88364f59b9
Android Studio <devandroid@ort>
2025-05-26 10:34:48 -0300

    creamos json + base para el nav

--- NutraMobile/App.js
+++ NutraMobile/App.js
@@ -1,14 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View, TextInput, Image, ImageBackground, SafeAreaView } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useFonts } from 'expo-font';
 import styles  from './assets/styles/styles';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 //npx expo install expo-font 
 const logo = require('./assets/images/LOGO-transparente.png')
 const imgPerfil = {uri: 'https://www.floatingwindturbineucm.com/wp-content/uploads/PERFIL-VACIO-1024x1024.png'};
 const iconoUbi = require('./assets/icons/iconoUbicacion.png')
+const Tab = createBottomTabNavigator();
+
+/*function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home"     component={StackANavigator} />
+      <Tab.Screen name="ECommerce" component={StackBNavigator} />
+      <Tab.Screen name="Recetas"   component={StackCNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person" size={24} color={color} />
+           ),
+        }}
+      />
+      <Tab.Screen name="Favoritos" component={StackBNavigator} />
+      <Tab.Screen name="Perfil" component={StackBNavigator} />
+    </Tab.Navigator>
+  );
+}
+
+ function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  ); HACER NAV QUE SOLO LLEVE A PERFIL*/
+
+
+
 export default function App() {
+
 
   useFonts({
     'Inter': require('./assets/fonts/Inter/Inter_18pt-Regular.ttf')
@@ -16,6 +48,7 @@ export default function App() {
 
   
   return (
+    
     <SafeAreaView style={styles.container}>
       
       <StatusBar style="auto" />
@@ -64,4 +97,7 @@ export default function App() {
     
   );
 }
+
+
+
 
